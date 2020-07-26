df098577d786a860dd24386e8d11afaa9620f667
OwenTsai <shiandai@gmail.com>
2020-07-27 01:25:53 +0800

    v 2.0 漸層功能

--- colorBabyShark/ViewController.swift
+++ colorBabyShark/ViewController.swift
@@ -12,58 +12,79 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var headColor: UIView!
     @IBOutlet weak var bodyColor: UIView!
+    
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redSliderShow: UILabel!
+    @IBOutlet weak var greenSliderShow: UILabel!
+    @IBOutlet weak var blueSliderShow: UILabel!
+    
     @IBOutlet weak var gradientSwitch: UISwitch!
-    @IBOutlet weak var redSliderValue: UILabel!
-    @IBOutlet weak var greenSliderValue: UILabel!
-    @IBOutlet weak var blueSliderValue: UILabel!
     @IBOutlet weak var originalButton: UIButton!
     @IBOutlet weak var randombutton: UIButton!
-    
-    //初始BabyShark顏色
-    var babySharkRed:Float = 254/255
-    var babySharkGreen:Float = 212/255
-    var babySharkBlue:Float = 3/255
-    var babySharkAlpha:Float = 1.0
     
     override func viewDidLoad(){
         super.viewDidLoad()
         
-        //將HeadColorView改形狀並設定初始顏色
+        //將HeadColorView改形狀並
         var image = UIImage(named: "baby_shark_head")
         let headView = UIImageView(image: image)
         headView.frame = headColor.bounds
         headView.contentMode = .scaleAspectFit
         headColor.mask = headView
-        headColor.backgroundColor = UIColor(red: CGFloat(babySharkRed), green: CGFloat(babySharkGreen), blue: CGFloat(babySharkBlue), alpha: 1)
         
-        //將BodyColorView改形狀並設定初始顏色
+        //將BodyColorView改形狀
         image = UIImage(named: "baby_shark_body")
         let bodyView = UIImageView(image: image)
         bodyView.frame = bodyColor.bounds
         bodyView.contentMode = .scaleAspectFit
         bodyColor.mask = bodyView
-        bodyColor.backgroundColor = UIColor(red: CGFloat(babySharkRed), green: CGFloat(babySharkGreen), blue: CGFloat(babySharkBlue), alpha: 1)
         
         //設定初始Slider數值
         redSlider.value = 254
         greenSlider.value = 212
         blueSlider.value = 3
-
+        colorChange()
+        
+    }
+    
+    func gradientColorChange(){
+        
+    }
+    
+    func colorChange() {
+        if gradientSwitch.isOn{
+            let headLayer = CAGradientLayer()
+            headLayer.frame = headColor.bounds
+            headLayer.colors = [CGColor(srgbRed: CGFloat(redSlider.value/255), green: CGFloat(greenSlider.value/255), blue: CGFloat(blueSlider.value/255), alpha: 1.0),CGColor(srgbRed: CGFloat(0.2), green: CGFloat(0.2), blue: CGFloat(0.2), alpha: 1.0)]
+            headLayer.startPoint = CGPoint(x: 0, y: 0)
+            headLayer.endPoint = CGPoint(x: 1, y: 1)
+            headColor.layer.addSublayer(headLayer)
+            headColor.backgroundColor = nil
+            
+            let bodyLayer = CAGradientLayer()
+            bodyLayer.frame = bodyColor.bounds
+            bodyLayer.colors = [CGColor(srgbRed: CGFloat(redSlider.value/255), green: CGFloat(greenSlider.value/255), blue: CGFloat(blueSlider.value/255), alpha: 1.0),CGColor(srgbRed: CGFloat(0.2), green: CGFloat(0.2), blue: CGFloat(0.2), alpha: 1.0)]
+            bodyColor.layer.addSublayer(bodyLayer)
+            bodyColor.backgroundColor = nil
+        }
+        
+        //變更頭部還有身體顏色
+        else {
+            headColor.backgroundColor = UIColor(red: CGFloat(redSlider.value/255), green: CGFloat(greenSlider.value/255), blue: CGFloat(blueSlider.value/255), alpha: 1.0)
+            bodyColor.backgroundColor = UIColor(red: CGFloat(redSlider.value/255), green: CGFloat(greenSlider.value/255), blue: CGFloat(blueSlider.value/255), alpha: 1.0)
+        }
+        //變更Slider顯示數字
+        redSliderShow.text = String(Int(redSlider.value))
+        greenSliderShow.text = String(Int(greenSlider.value))
+        blueSliderShow.text = String(Int( blueSlider.value))
     }
     
     //設定Slider調整顏色
     @IBAction func colorChange(_ sender: UISlider) {
-        babySharkRed = redSlider.value/255
-        babySharkBlue = blueSlider.value/255
-        babySharkGreen = greenSlider.value/255
-        headColor.backgroundColor = UIColor(red: CGFloat(babySharkRed), green: CGFloat(babySharkGreen), blue: CGFloat(babySharkBlue), alpha: 1)
-        bodyColor.backgroundColor = UIColor(red: CGFloat(babySharkRed), green: CGFloat(babySharkGreen), blue: CGFloat(babySharkBlue), alpha: 1)
-        redSliderValue.text = String(Int(redSlider.value))
-        greenSliderValue.text = String(Int(greenSlider.value))
-        blueSliderValue.text = String(Int( blueSlider.value))
+        colorChange()
     }
     
     //原始顏色
@@ -71,23 +92,27 @@ class ViewController: UIViewController {
         redSlider.value = 254
         greenSlider.value = 212
         blueSlider.value = 3
-        headColor.backgroundColor = UIColor(red: CGFloat(redSlider.value/255), green: CGFloat(greenSlider.value/255), blue: CGFloat(blueSlider.value/255), alpha: 1)
-        bodyColor.backgroundColor = UIColor(red: CGFloat(redSlider.value/255), green: CGFloat(greenSlider.value/255), blue: CGFloat(blueSlider.value/255), alpha: 1)
-        redSliderValue.text = String(Int(redSlider.value))
-        greenSliderValue.text = String(Int(greenSlider.value))
-        blueSliderValue.text = String(Int( blueSlider.value))
+        colorChange()
     }
     
     //隨機顏色
-    @IBAction func randomColor(_ sender: Any) {
+    @IBAction func randomColor(_ sender: UIButton) {
         redSlider.value = Float.random(in: 0...255)
         blueSlider.value = Float.random(in: 0...255)
         greenSlider.value = Float.random(in: 0...255)
-        headColor.backgroundColor = UIColor(red: CGFloat(redSlider.value/255), green: CGFloat(greenSlider.value/255), blue: CGFloat(blueSlider.value/255), alpha: 1)
-        bodyColor.backgroundColor = UIColor(red: CGFloat(redSlider.value/255), green: CGFloat(greenSlider.value/255), blue: CGFloat(blueSlider.value/255), alpha: 1)
-        redSliderValue.text = String(Int(redSlider.value))
-        greenSliderValue.text = String(Int(greenSlider.value))
-        blueSliderValue.text = String(Int( blueSlider.value))
+        colorChange()
+    }
+    
+    //漸層顏色
+    @IBAction func gradientSwitch(_ sender: UISwitch){
+        if gradientSwitch.isOn{
+            colorChange()
+        }
+        else {
+            headColor.layer.sublayers?.removeAll()
+            bodyColor.layer.sublayers?.removeAll()
+            colorChange()
+        }
     }
 }
 
